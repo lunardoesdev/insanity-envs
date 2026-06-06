75d2164c4f25f98fed10e823de3062080e7d2153
Lunar <lunarlifeburst@gmail.com>
2026-06-06 14:10:56 +1000

    init

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Litoli CMake/Nix cross-compilation example";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+      ...
+    }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+      rustOverlay = (import rust-overlay);
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ rustOverlay ];
+          };
+        in
+        {
+          default = pkgs.callPackage ./envs/litoli { };
+        }
+      );
+
+      formatter = forAllSystems (system: (import nixpkgs { inherit system; }).nixfmt-tree);
+    };
+}
